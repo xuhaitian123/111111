3ff78ba6a3791975ff64194903e655110c561094
liguangsong <863925367@qq.com>
2018-09-17 18:48:47 +0800

    map page add car number show

--- src/views/Map/Map.vue
+++ src/views/Map/Map.vue
@@ -8,18 +8,24 @@
       </div>
       <div class="map-contaienr1">
         <button  class="button-2" v-on:click="test2"></button>
-        <button  style="position: absolute;left: 400px;z-index:200" v-on:click="test4">test4</button>
         <img class="two" src="../../../static/map/地图22.png"/>
-
-
       </div>
       <div class="map-contaienr2">
         <button  class="button-3" v-on:click="enter"></button>
         <img class="three" src="../../../static/map/地图33.png"/>
       </div>
-      <button  style="position: absolute;left: 500px" v-on:click="home">home</button>
-      <button  style="position: absolute;left: 500px" v-on:click="enter">进入</button>
+    </div>
+    <div class="map-area-container">
+      <div class="map-area-container-header">
+        <div class="map-area-container-header-action"><img  src="/static/map/left.png"/></div>
+        <div class="map-area-container-header-title">
+          <div>中国 </div><img src="/static/map/right.png"> 江苏省 > 淮安市
+        </div>
 
+      </div>
+      <div class="map-area-container-main">
+
+      </div>
     </div>
   </div>
 
@@ -58,10 +64,41 @@
     width: 100%;
     overflow: hidden;
     background: black;
+    position: relative;
   }
+  .map-area-container{
+    position: absolute;
+    width: 320px;
+    height: 320px;
+    left: 30px;
+    top: 30px;
+    background: rgb(56,58,71);
+  }
+  .map-area-container-header{
+    height: 40px;
+    background: rgb(39,39,53);
+    display: flex;
+    align-items: center;
+    padding: 0 16px;
+  }
+  .map-area-container-header-action img{
+    width: 22px;
+    height: 14px;
+  }
+  .map-area-container-header-title{
+    display: flex;
+    align-items: center;
+  }
+  .map-area-container-header-title img{
+    width: 16px;
+    height: 16px;
+  }
+  .map-area-container-header-title{
+    padding-left: 12px;
+  }
+
   .container{
     margin: auto;
-
   }
   .map-contaienr{}
   .container{
@@ -151,7 +188,8 @@
       test1(){
         $('.map-contaienr').animate({width:'500%', height:'500%', left:'-340%','top':'-240%',opacity:'0.4'},  500, ()=>{
           $('.map-contaienr').animate({opacity: '0'},500,function(){
-            $('.map-contaienr').animate({width:'100%', height:'100%', left:'0','top':'0'})
+
+              $('.map-contaienr').animate({width:'100%', height:'100%', left:'0','top':'0'})
           })
           $('.map-contaienr1').animate({opacity: '1'},500)
           // this.test2()
@@ -160,6 +198,7 @@
       test2(){
         $('.map-contaienr1').animate({width:'200%', height:'200%', left:'-50%','top':'-50%',opacity:'0.4'},  500, ()=>{
           $('.map-contaienr1').animate({opacity: '0'},300, function(){
+
             $('.map-contaienr1').animate({width:'100%', height:'100%', left:'0','top':'0'})
           })
           $('.map-contaienr2').animate({opacity: '1'},1000)
