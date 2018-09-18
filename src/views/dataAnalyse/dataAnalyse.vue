502c021042bc9d27b1fa03fbc0c5ee19acc54d61
xiaofei112233 <937136979@qq.com>
2018-09-18 19:00:33 +0800

    添加数据分析页面1

---
+++ src/views/dataAnalyse/dataAnalyse.vue
@@ -0,0 +1,19 @@
+<template>
+<div >aaaaaa</div>
+</template>
+
+<script>
+    export default {
+        name: "signal lamp",
+      methods:{
+
+      },
+      mounted(){
+          console.log('1212')
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
