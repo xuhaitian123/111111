bf4b94f3afd0e86e174a1911bc456c83804223d8
liuchaoxue <482739566@qq.com>
2018-09-18 22:27:37 +0800

    flex

--- src/views/Dashboard/IntersectionsMap.vue
+++ src/views/Dashboard/IntersectionsMap.vue
@@ -16,24 +16,34 @@
             <div class="Road_row_link">
               <el-row type="flex" justify="space-around">
                 <el-col :span="5">
-                  <div class="fs14">交叉口 <span>{{linkData.data.link.link_name}}</span></div>
+                  <div class="fs14">交叉口 <span>{{nodeData.node.node_name}}</span></div>
                 </el-col>
                 <el-col :span="5">
-                  <div class="fs14">总流量 <span>{{linkData.data.link.link_lane_capacity}}vph</span></div>
+                  <div class="fs14">总流量 <span>{{nodeData.flow}}vph</span></div>
                 </el-col>
                 <el-col :span="8">
                   <div class="fs14" style="display: flex">
                     交叉口拥堵水平
                     <ul style="line-height: 30px;margin-left: 30px">
                       <li>
-                        <div
-                          style="width: 17px;height: 17px;background: green;float: left;border-radius: 50%;margin: 7px 10px 0 10px"></div>
-                        <div
-                          style="width: 17px;height: 17px;background: #e7c936;float: left;border-radius: 50%;margin: 7px 10px 0 10px"></div>
-                        <div
-                          style="width: 17px;height: 17px;background: darkorange;float: left;border-radius: 50%;margin: 7px 10px 0 10px"></div>
-                        <div
-                          style="width: 17px;height: 17px;background: red;float: left;border-radius: 50%;margin: 7px 10px 0 10px"></div>
+                        <div class="Road_border" :style="{border: nodeData.value <30 ? '1px solid green' :'0' }">
+                          <div class="Road_circle"
+                               style="background: green;"></div>
+                        </div>
+                        <div class="Road_border"
+                             :style="{border: nodeData.value <50&&nodeData.value >30 ? '1px solid #e7c936' :'0' }">
+                          <div class="Road_circle"
+                               style="background: #e7c936;"></div>
+                        </div>
+                        <div class="Road_border"
+                             :style="{border: nodeData.value <60&&nodeData.value >50 ? '1px solid darkorange' :'0' }">
+                          <div class="Road_circle"
+                               style="background: darkorange;"></div>
+                        </div>
+                        <div class="Road_border" :style="{border: nodeData.value >60 ? '1px solid red' :'0' }">
+                          <div class="Road_circle"
+                               style="background: red;"></div>
+                        </div>
                       </li>
                     </ul>
                   </div>
@@ -50,7 +60,7 @@
             <div class="Node_right_top">
               <el-tabs type="border-card" style="height: 420px;background: #353644">
                 <el-tab-pane label="当前信号灯配时方案">
-                  <el-row style="padding: 40px 20px 0 20px;text-align: center">
+                  <el-row style="padding: 40px 20px 0;text-align: center">
                     <el-col :span="3">
                       <div class="" style="margin-bottom: 5px">东</div>
                       <div style="height: 86px;border-radius: 10px;width: 30px;background: #2a2b36;margin: auto">
@@ -68,6 +78,49 @@
                       <div class="">南</div>
                     </el-col>
                   </el-row>
+                  <div style="padding: 60px 20px 0;">
+                    <div
+                      style="border-bottom: 1px solid #c9c9cc;border-top: 1px solid #c9c9cc;font-size: 12px;padding-bottom: 10px">
+                      <el-row>
+                        <el-col :span="10">
+                          <div class="">
+                            <div>
+                              <div style="width: 10px;height: 10px;border-radius: 50%;background: red;float: left;margin-top: 2px;
+    margin-right: 10px;"></div>
+                              <div>↑ 02(R)</div>
+
+                              <div style="height: 12px;background: green;width: 90%;float: left">71s</div>
+                              <div style="height: 12px;background: darkorange;width: 10%;float: left"></div>
+                            </div>
+                          </div>
+                        </el-col>
+                        <el-col :span="4">
+                          <div>
+                            <div>→ 02</div>
+
+                            <div style="height: 12px;background: green;width: 90%;float: left">35s</div>
+                            <div style="height: 12px;background: darkorange;width: 10%;float: left"></div>
+                          </div>
+                        </el-col>
+                        <el-col :span="7">
+                          <div>
+                            <div>→ 02</div>
+
+                            <div style="height: 12px;background: green;width: 90%;float: left">27s</div>
+                            <div style="height: 12px;background: darkorange;width: 10%;float: left"></div>
+                          </div>
+                        </el-col>
+                        <el-col :span="3">
+                          <div>
+                            <div>↓ 02</div>
+
+                            <div style="height: 12px;background: green;width: 90%;float: left">20s</div>
+                            <div style="height: 12px;background: darkorange;width: 10%;float: left"></div>
+                          </div>
+                        </el-col>
+                      </el-row>
+                    </div>
+                  </div>
                 </el-tab-pane>
                 <el-tab-pane label="交叉口饱和度/排队长度数据展示">
                   <el-row>
@@ -104,9 +157,56 @@
                         </div>
                       </div>
                     </el-col>
-                    <el-col :span="18">
-                      <div>
+                    <el-col :span="2" style="padding-top: 170px;font-size: 12px">
+                      南
+                      <div style="padding-top: 20px"> →</div>
+                    </el-col>
+                    <el-col :span="14">
+                      <div style="font-size: 12px;padding-left: 38%;">↓ 人民路</div>
+                      <div style="display: flex;flex-wrap: wrap;">
+                        <div class="block" style="text-align: center">
+                          <div style="padding-top: 50%;letter-spacing: 10px"><span>></span> <span>25</span>
+                            <span>12</span> <span>0</span>
+                          </div>
+                          <div style="letter-spacing: 10px"> | | | |</div>
+                        </div>
+                        <div class="block" style="border-left: 3px solid #c9c9cc;border-right: 3px solid #c9c9cc"></div>
+                        <div class="block">
+                          <div class="Node_length_list Node_length_list_right">
+                            <ul class="">
+                              <li><span>--</span>>50</li>
+                              <li><span>--</span>25-37</li>
+                              <li><span>--</span>12-25</li>
+                              <li><span>--</span>0-12</li>
+                            </ul>
+                          </div>
+                        </div>
+                        <div class="block" style="border-top: 3px solid #c9c9cc;border-bottom: 3px solid #c9c9cc"></div>
+                        <div class="block" style="border: 3px solid #c9c9cc;"></div>
+                        <div class="block" style="border-top: 3px solid #c9c9cc;border-bottom: 3px solid #c9c9cc"></div>
+                        <div class="block">
+                          <div class="Node_length_list Node_length_list_left" style="text-align: right">
+                            <ul class="">
+                              <li>0-12 <span>--</span></li>
+                              <li>12-25 <span>--</span></li>
+                              <li>25-37 <span>--</span></li>
+                              <li>>50 <span>--</span></li>
+                            </ul>
+                          </div>
+                        </div>
+                        <div class="block" style="border-left: 3px solid #c9c9cc;border-right: 3px solid #c9c9cc"></div>
+                        <div class="block" style="padding-left: 15px">
+                          <div style="letter-spacing: 10px"> | | | |</div>
+                          <div style="letter-spacing: 10px"><span>0</span> <span>12</span> <span>25</span>
+                            <span> > </span></div>
+                        </div>
+                        <div style="font-size: 12px;padding-left: 38%;">人民路 ↑</div>
                       </div>
+                    </el-col>
+                    <el-col :span="2" style="font-size: 12px">
+                      <div style="font-size: 16px">N</div>
+                      <div style="padding-top: 130px;padding-bottom: 20px"> ←</div>
+                      南
                     </el-col>
                   </el-row>
                 </el-tab-pane>
@@ -169,9 +269,8 @@
     data() {
       return {
         roadNetCongestionScore: 0,
-        linkData: {
-          data: {link: {}},
-          related_data: {}
+        nodeData: {
+          node: {}
         },
       }
     },
@@ -181,14 +280,27 @@
     methods: {
       init() {
         this.getNodeCongestionSource();
-        this.getNodeDataD5ByNodeId();
-        this.getNodeDataD12ByNodeId();
         this.getRoadNetCongestionScore();
+        this.getCurrentSignalByNodeId()
       },
-      getNodeCongestionSource() { //交叉口拥堵评分
-        this.$http.get('/trafficCongestion/nodeCongestionSource?current=true&nodeId=1')
+      getNodeDataD5ByNodeId(nodeId) {  //机动车流量 + 非机动车流量
+        return new Promise((resolve, reject) => {
+          this.$http.get('/nodeData/getNodeDataD5ByNodeId?nodeId=' + nodeId + '&current=true')
+            .then((response) => {
+              this.$http.get('/nodeData/getNodeDataD6ByNodeId?nodeId=' + nodeId + '&current=true')
+                .then((result) => {
+                  resolve(response.data.value + result.data.value)
+                })
+            })
+        });
+      },
+      getNodeCongestionSource() { //交叉口延误数据
+        this.$http.get('/nodeData/getNodeDataD12ByNodeId?nodeId=' + this.$route.params.id + '&current=true')
           .then((response) => {
-            // console.log(response)
+            this.getNodeDataD5ByNodeId(response.data.node_id).then((data) => {
+              response.data.flow = data;
+              this.nodeData = response.data;
+            });
           })
       },
       getRoadNetCongestionScore() { //路网拥堵评分
@@ -198,14 +310,18 @@
             this.roadNetCongestionScore = response.data.value;
           })
       },
-      getNodeDataD5ByNodeId() { //交叉口机动车流量
-        this.$http.get('nodeData/getNodeDataD5ByNodeId?nodeId=2&current=true')
+      getCurrentSignalByNodeId() {  //实时红绿灯
+        this.$http.get('/signal/currentSignalByNodeId?nodeId=2')
           .then((response) => {
             console.log(response)
           })
-      },
-      getNodeDataD12ByNodeId() { //交叉口延误数据
-        this.$http.get('/nodeData/getNodeDataD12ByNodeId?nodeId=2&current=true')
+
+        this.$http.get('/signal/currentSignalByLinkId?linkId=201')
+          .then((response) => {
+            console.log(response)
+          })
+
+        this.$http.get('/signal/currentSignalByMovementId?movementId=201')
           .then((response) => {
             console.log(response)
           })
@@ -215,6 +331,21 @@
 </script>
 
 <style scoped>
+  .Road_border {
+    width: 19px;
+    height: 19px;
+    border-radius: 50%;
+    float: left;
+    margin: 7px 10px 0 10px;
+  }
+
+  .Road_circle {
+    width: 17px;
+    height: 17px;
+    border-radius: 50%;
+    margin: 1px 0 0 1px
+  }
+
   .Dashboard_box_card {
     border-radius: 1px;
     color: white !important;
@@ -299,6 +430,30 @@
     color: #c9c9cc;
     padding: 10px 0;
     font-size: 12px;
+  }
+
+  .block {
+    height: 110px;
+    width: 30%;
+    color: #c9c9cc;
+    line-height: 25px;
+    font-size: 12px;
+  }
+
+  .block span {
+    letter-spacing: 0;
+  }
+
+  .Node_length_list {
+    padding: 0 20px;
+  }
+
+  .Node_length_list_right span {
+    margin-right: 10px;
+  }
+
+  .Node_length_list_left span {
+    margin-left: 10px;
   }
 
 </style>
