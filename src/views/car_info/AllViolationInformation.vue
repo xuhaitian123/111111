159c97c11570e0e28ce358e3df6555ed1c7ee8d7
xuhaitian123 <1397548794@qq.com>
2019-03-13 19:30:55 +0800

    增加新的图表

---
+++ src/views/car_info/AllViolationInformation.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <Area></Area>
+    <el-table
+    :data="tableData"
+    style="width: 90%;"
+    max-height="800"
+    :row-class-name="tableRowClassName"
+    :default-sort = "{prop: 'date', order: 'descending'}"
+    >
+    <el-table-column
+      prop="car_name"
+      label="车辆名称"
+      sortable
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="car_type"
+      label="车辆类型"
+      sortable
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="car_color"
+      label="车辆颜色">
+    </el-table-column>
+    <el-table-column
+      prop="license_num"
+      label="车牌号"
+      sortable
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="people_name"
+      label="车主姓名"
+      sortable
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="is_check"
+      label="是否年检">
+    </el-table-column>
+    <el-table-column
+      prop="is_accident"
+      label="是否发生过事故"
+      sortable
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="reg_time"
+      label="登记时间"
+      sortable
+      width="180">
+    </el-table-column>
+  </el-table>
+  <div class="block">
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="40">
+    </el-pagination>
+  </div>
+  </div>
+</template>
+
+<script>
+import Area from "../../components/Area/Area";
+
+export default {
+  name: "AllViolationInformation",
+  data() {
+    return {
+      data: [],
+      tableData: [],
+    };
+  },
+  methods: {
+    tableRowClassName({row, rowIndex}){
+        if(row.is_accident == "是"){
+            return "accident"
+        }
+        console.log(row)
+        return '' ;
+    },
+    get_pie_data() {
+      var this_ = this;
+      this.$http.post("/AllViolationInformation/allData",
+        {
+          username: this.username,
+          password: this.password
+        })
+        .then(data => {
+            console.log(data)
+          this_.tableData = data.data;
+          this_.count = data.data.count
+        //   this_.init()
+          console.log(this_.tableData);
+        });
+    },
+  },
+  components: {
+    Area
+  },
+  created() {
+    this.get_pie_data();
+  },
+  mounted() {
+
+  }
+};
+</script>
+<style scoped>
+#pie {
+  width: 500px;
+  height: 400px;
+  background-color: #1D1D2C;
+}
+.block{
+    float: right;
+    margin:  10px 20px 0 0;
+}
+.accident{
+    background-color:red;
+}
+</style>
+
+<style>
+.el-pagination .el-select .el-input{
+    margin: 0 !important;
+    width: 100% !important
+}
+.accident{
+    background-color:red;
+}
+</style>
+
--- src/views/car_info/VisualChart.vue
+++ src/views/car_info/VisualChart.vue
@@ -2,6 +2,7 @@
   <div>
     <Area></Area>
     <div id="pie"></div>
+    <div id="bar"></div>
   </div>
 </template>
 
@@ -19,6 +20,7 @@ export default {
         { value: 235, name: "视频广告" },
         { value: 400, name: "搜索引擎" }
       ],
+      bar_data :[],
       count : 0,
       text: "不同类型车辆保有量对比"
     };
@@ -32,14 +34,26 @@ export default {
           password: this.password
         })
         .then(data => {
-            console.log(data)
           this_.data = data.data.data;
           this_.count = data.data.count
-          this_.init()
-          console.log(this_.data.data);
+          this_.init1()
         });
     },
-    init() {
+    get_bar_data() {
+      var this_ = this;
+      this.$http.post("/reg_car_statistics",
+        {
+          username: this.username,
+          password: this.password,
+          start_time : '2001-03-04 00:00:00',
+          end_time : '2001-03-30 23:00:00'
+        })
+        .then(data => {
+          this_.bar_data = data.data;
+          this_.init2()
+        });
+    },
+    init1() {
       this.myChart = this.$echarts.init(document.getElementById("pie"));
       var option = {},
         option = {
@@ -48,6 +62,15 @@ export default {
                 subtext: '共有' + this.count + '辆',
                 x:'center'
             },
+            toolbox: {
+              // y: 'bottom',
+              feature: {
+                // dataView: {},
+                saveAsImage: {
+                  pixelRatio: 2
+                }
+              }
+          },
             tooltip : {
                 trigger: 'item',
                 formatter: "{a} <br/>{b} : {c} ({d}%)"
@@ -79,6 +102,94 @@ export default {
             ]
         };
       this.myChart.setOption(option, true);
+    },
+    init2() {
+      this.myChart = this.$echarts.init(document.getElementById("bar"));
+      var xAxisData = this.bar_data.all_date;
+      var option = {},
+        option = {
+          title: {
+              text: '柱状图动画延迟'
+          },
+          legend: {
+              data: ['警车', '私家车','出租车','救护车','公交车','政府用车'],
+              align: 'left'
+          },
+          toolbox: {
+              // y: 'bottom',
+              feature: {
+                dataZoom: {
+                  yAxisIndex: 'none'
+                },
+                restore: {},
+                magicType: {
+                    type: ['stack', 'tiled']
+                },
+                dataView: {},
+                saveAsImage: {
+                  pixelRatio: 2
+                }
+              }
+          },
+          tooltip: {},
+          xAxis: {
+              data: xAxisData,
+              silent: false,
+              splitLine: {
+                  show: false
+              }
+          },
+          yAxis: {
+          },
+          series: [{
+              name: '警车',
+              type: 'bar',
+              data: this.bar_data.data[0],
+              animationDelay: function (idx) {
+                  return idx * 10;
+              }
+          }, {
+              name: '私家车',
+              type: 'bar',
+              data: this.bar_data.data[1],
+              animationDelay: function (idx) {
+                  return idx * 10 + 100;
+              }
+          },{
+              name: '出租车',
+              type: 'bar',
+              data: this.bar_data.data[2],
+              animationDelay: function (idx) {
+                  return idx * 10 + 100;
+              }
+          },{
+              name: '救护车',
+              type: 'bar',
+              data: this.bar_data.data[3],
+              animationDelay: function (idx) {
+                  return idx * 10 + 100;
+              }
+          },{
+              name: '公交车',
+              type: 'bar',
+              data: this.bar_data.data[4],
+              animationDelay: function (idx) {
+                  return idx * 10 + 100;
+              }
+          },{
+              name: '政府用车',
+              type: 'bar',
+              data: this.bar_data.data[5],
+              animationDelay: function (idx) {
+                  return idx * 10 + 100;
+              }
+          }],
+          animationEasing: 'elasticOut',
+          animationDelayUpdate: function (idx) {
+              return idx * 5;
+          }
+      };
+      this.myChart.setOption(option, true);
     }
   },
   components: {
@@ -86,6 +197,7 @@ export default {
   },
   created() {
     this.get_pie_data();
+    this.get_bar_data();
   },
   mounted() {}
 };
@@ -94,6 +206,11 @@ export default {
 #pie {
   width: 500px;
   height: 400px;
+  background-color: #1D1D2C;
+}
+#bar{
+  width: 700px;
+  height: 500px;
   background-color: #1D1D2C;
 }
 </style>
